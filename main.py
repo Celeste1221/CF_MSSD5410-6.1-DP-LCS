18c8882d0f9f3ddebc0e26294940e30b414e3125
Celeste <celeste1221@gmail.com>
2022-03-01 00:27:13 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from algorithms import *
-import string
 
 
-# compare the the code from the internet with the modified code to see how much is similar
+# Compares code copied from the internet with modifications done in class to see how much is similar
 
 # read in file, clean it up to ready if for comparison to another file
 def process_file(fname, enc):
     # open file for reading 'r'
     with open(fname, 'r', encoding=enc) as file:
-        dat = file.read()  # TODO: get rid of comments
-        return dat.split()  # return read data sans whitespace
+        dat = file.readlines()
+        dat_str = ""
+        # get rid of comments
+        for line in dat:
+            # ref: https://stackoverflow.com/questions/1706198/python-how-to-ignore-comment-lines-when-reading-in-a-file
+            # returns the first index of a tuple of (right side, "separator", left side).
+            right = line.partition("#")[0]
+            if len(right) > 0:  # if the line is not empty
+                dat_str += str(right)  # add it to the new data string
+    return dat_str.split()
 
 
 def main():
@@ -21,20 +28,20 @@ def main():
     edit_dist_code = process_file(edit_distf, "utf-8")
     class_code = process_file(classf, "utf-8")
 
-    # print(lcs_code[:25])
+    # for testing:
+    # print(lcs_code)
 
-    print("The LSC between {0} and {1} is {2}".format(classf, lcsf, lcs(class_code, lcs_code)))
-    print("The LSC between {0} and {1} is {2}".format(classf, edit_distf, lcs(class_code, edit_dist_code)))
+    print("\nThe LCS between the modifications done in class and the original lcs code copied from online"
+          " is {0}".format(lcs(class_code, lcs_code)))
+    print("The LCS between the modifications done in class and the original edit distance code copied from online"
+          " is {0}".format(lcs(class_code, edit_dist_code)))
 
-    print("The edit distance between {0} and {1} is {2}".format(classf, lcsf, editDistDP(class_code,
-                                                                                         lcs_code,
-                                                                                         len(class_code),
-                                                                                         len(lcs_code))))
+    print("\nThe edit distance between the modifications done in class and the original lcs code copied from online"
+          " is {0}".format(editDistDP(class_code, lcs_code, len(class_code), len(lcs_code))))
 
-    print("The edit distance between {0} and {1} is {2}".format(classf, edit_distf, editDistDP(class_code,
-                                                                                               edit_dist_code,
-                                                                                               len(class_code),
-                                                                                               len(edit_dist_code))))
+    print("The edit distance between the modifications done in class and the original edit distance code "
+          "copied from online is {0}".format(editDistDP(class_code, edit_dist_code, len(class_code),
+                                                        len(edit_dist_code))))
 
 
 if __name__ == "__main__":
